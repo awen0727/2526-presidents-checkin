f7d53a71718dea108a4ad0400962a3918f9c599f
Chris Wang <chriswang@Mac.localdomain>
2026-06-20 19:44:07 +0800

    更新 Apps Script 部署網址

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.PRESIDENTS_CHECKIN_CONFIG = {
-  apiUrl: "https://script.google.com/macros/s/AKfycbzJbVQWv8owI58XJIuTl9BcFlnWWVrYNGBu6yK3xES4O33dGTPljePBliq8q9Nbe-oU/exec",
+  apiUrl: "https://script.google.com/macros/s/AKfycbx6l1mfhODV_CwPaMQhdLaHrDHw7Vn4IOYAhmVJo1rqict2yq-qGthcR2OLj5J1VbKm/exec",
   liffId: "2010452724-MvUou0rS"
 };
