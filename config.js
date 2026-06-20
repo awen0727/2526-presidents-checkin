a0ac9249ddc92a2a8227291df66f3797ec6d074b
Chris Wang <chriswang@ChrisdeMacBook-Pro.local>
2026-06-20 09:02:24 +0800

    設定2526會長聯誼會 LIFF

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.PRESIDENTS_CHECKIN_CONFIG = {
   apiUrl: "https://script.google.com/macros/s/AKfycbwrwD4U90F0XlxZ1A0kGFqHczqvmjUOG0kHtoYll0zWLMQpeiEPZNTHHFRbjxgUNxHT/exec",
-  liffId: "PASTE_LIFF_ID"
+  liffId: "2010452724-MvUou0rS"
 };
