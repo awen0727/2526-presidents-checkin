b86d553fe7335b80a52144b402a41b47007a6eef
Chris Wang <chriswang@ChrisdeMacBook-Pro.local>
2026-06-22 01:13:57 +0800

    切換前端至 attendance-7 部署

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.PRESIDENTS_CHECKIN_CONFIG = {
-  apiUrl: "https://script.google.com/macros/s/AKfycbyRMp8UXsTLfWFfc405IExML15tDaikjlpZFyBwDXI9BMd2eKYYhVWHCqaejsb9EJ-E/exec",
+  apiUrl: "https://script.google.com/macros/s/AKfycbzIr_yTAM-fIwGSZaDmaYdj4n_YnpSD2HwBeB2OgDYi-c088tm_8WWobWQJ5N3CWGkB/exec",
   liffId: "2010452724-MvUou0rS"
 };
