4597151d70e3074ead0c02bfe3c3f681a01894d6
Chris Wang <chriswang@ChrisdeMacBook-Pro.local>
2026-07-04 03:07:39 +0800

    切換至會長 LINE ID 後端

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.PRESIDENTS_CHECKIN_CONFIG = {
-  apiUrl: "https://script.google.com/macros/s/AKfycbxSSFuEDN0uMo09cRSFwQlaTEcjOCeEmPTpE8bO0j1kxSnbCCSN7yMkDa5sOPUBYv7w/exec",
+  apiUrl: "https://script.google.com/macros/s/AKfycbx-ARH0ez5KJztlT2v3w3JAzFRkWovglYQeTotZW2R79ZRLGFCui9zktEO8UE0EcgL1/exec",
   liffId: "2010452724-MvUou0rS"
 };
