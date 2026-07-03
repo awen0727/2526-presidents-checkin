5ee690efbfb80ad7848fe77a1d18da434129b233
Chris Wang <chriswang@ChrisdeMacBook-Pro.local>
2026-07-03 23:25:14 +0800

    切換前端至 line-oa-9 部署

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.PRESIDENTS_CHECKIN_CONFIG = {
-  apiUrl: "https://script.google.com/macros/s/AKfycbzIr_yTAM-fIwGSZaDmaYdj4n_YnpSD2HwBeB2OgDYi-c088tm_8WWobWQJ5N3CWGkB/exec",
+  apiUrl: "https://script.google.com/macros/s/AKfycbwPMpm5a3E5fiZRqoE_t_dubh1vHFB3Vgn2JjR4dxhC4QzRqOOLgEJ_C8p_klsKYUk/exec",
   liffId: "2010452724-MvUou0rS"
 };
