154afc7c5032ed9480c8870fda59c5330d7a003a
Chris Wang <chriswang@Mac.localdomain>
2026-07-25 00:19:19 +0800

    切換至 LINE 提及指令後端

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.PRESIDENTS_CHECKIN_CONFIG = {
-  apiUrl: "https://script.google.com/macros/s/AKfycbxMv4fh4RDnvyId30XdAQl7aSHOTGHfSXRvBn7LYqpy5M3aCjEMem7BuHFdIWDXokQw/exec",
+  apiUrl: "https://script.google.com/macros/s/AKfycby3TiVLmzrEVKANYc_dwPwHyZPyCnftRkQUNGz8eAgvoPvMnytbPFzXpMJMaTBIiZnP/exec",
   liffId: "2010452724-MvUou0rS"
 };
