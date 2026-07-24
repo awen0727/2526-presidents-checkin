836c05eb67a979a001814aa6be9780289fdd4fb9
Chris Wang <chriswang@Mac.localdomain>
2026-07-25 00:03:04 +0800

    切換至本月壽星後端

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.PRESIDENTS_CHECKIN_CONFIG = {
-  apiUrl: "https://script.google.com/macros/s/AKfycbyRryDD1tFJgBc0-uwMwUtbxjEQXN-pI3Q_PbjOZg2fkQfhbaVLrM-tLJdvYbNxnHD4/exec",
+  apiUrl: "https://script.google.com/macros/s/AKfycbxMv4fh4RDnvyId30XdAQl7aSHOTGHfSXRvBn7LYqpy5M3aCjEMem7BuHFdIWDXokQw/exec",
   liffId: "2010452724-MvUou0rS"
 };
