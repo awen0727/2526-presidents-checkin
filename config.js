872bd923e41fd1213b9cd692258366d001d0006f
Chris Wang <chriswang@ChrisdeMacBook-Pro.local>
2026-07-28 01:58:33 +0800

    切換至顧問生日後端

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.PRESIDENTS_CHECKIN_CONFIG = {
-  apiUrl: "https://script.google.com/macros/s/AKfycbwsrmbc7nuYVyhn12BQfJcKkhWrdWoJ8qjH-2pE3Lv4YMCjjlI1tmnJstIobRtoltfF/exec",
+  apiUrl: "https://script.google.com/macros/s/AKfycbyiNzEt-XyjG7rlbWYIzsoJMDZIZPjiESa6oyWJmfrlot7H4DSfJPxrJ2wH5zVe5muW/exec",
   liffId: "2010452724-MvUou0rS"
 };
