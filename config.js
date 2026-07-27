543712be3c0c47f6cafc6fadbb8008bb6555d51a
Chris Wang <chriswang@ChrisdeMacBook-Pro.local>
2026-07-28 00:52:30 +0800

    切換至活動補登後端

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.PRESIDENTS_CHECKIN_CONFIG = {
-  apiUrl: "https://script.google.com/macros/s/AKfycbz_nGWGex9fRw7QAotqzYcHZHNzyqB2iNwNQmJ90ilb_gHhxBnQfwFI0Sun4QOYr9oy/exec",
+  apiUrl: "https://script.google.com/macros/s/AKfycbwsrmbc7nuYVyhn12BQfJcKkhWrdWoJ8qjH-2pE3Lv4YMCjjlI1tmnJstIobRtoltfF/exec",
   liffId: "2010452724-MvUou0rS"
 };
