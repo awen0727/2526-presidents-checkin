3f7e36f4335d6ef94fbb5a19926ea31627392c4f
Chris Wang <chriswang@ChrisdeMacBook-Pro.local>
2026-07-28 03:30:55 +0800

    網站改連新版 Apps Script API

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.PRESIDENTS_CHECKIN_CONFIG = {
-  apiUrl: "https://script.google.com/macros/s/AKfycbyiNzEt-XyjG7rlbWYIzsoJMDZIZPjiESa6oyWJmfrlot7H4DSfJPxrJ2wH5zVe5muW/exec",
+  apiUrl: "https://script.google.com/macros/s/AKfycbzXoeZ9lWm5X1tEkA8Blx76r49rVL0Jsn5Vm9STwfDiM4HcgGzsGa92QvT0idbfDAiD/exec",
   liffId: "2010452724-MvUou0rS"
 };
